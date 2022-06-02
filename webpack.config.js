69caa4a6ace7ca66f438f2b2f65bae29a11d9487
Slavavw <slavavw1978@gmail.com>
2022-06-02 18:57:25 +0300

    add webpack in proeject

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,52 @@
-const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+
+console.log(path.join(__dirname,'/src','index.html'));
+let mode = 'development';
+if (process.env.NODE_ENV === 'production') mode = 'production';
+
+console.log(mode);
 
 module.exports = {
-  entry: './main/index.js',
-  output: {
-    filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'shelter'),
-    assetModuleFilename: 'assets/[hash][ext]'
+ mode: mode,
+ entry: './src/index.js',
+ output: {
+  filename: "[name].[contenthash].js",
+ assetModuleFilename: 'assets/[hash][ext][query]', //модуль ресурсов куда будут скидываться файлы рксурсов img etc..
+ clean: true, //указываем чтобы папка dist очищалась каждый раз при сборке
+},
+ plugins:[
+  new MiniCssExtractPlugin({filename:'[name].[contenthash].css'}),
+  new htmlWebpackPlugin({template:path.join(__dirname,'main','index.html')}),
+  new CleanWebpackPlugin(),
+ ],
+ module:{
+  rules: [
+   { 
+     test: /\.html$/i,
+     use: 'html-loader', //обработки изображений в файлах html
+   },
+   {
+    test: /\.(sa|sc|c)ss$/i,
+    use:[mode==='development'?'style-loader':MiniCssExtractPlugin.loader,
+    //для development  стили будут встраиваться в папку head  нашего index.html (отрабатывает style-loader)
+    //для production будет создаваться файл стилей отрабатывает лоудер MiniCssExtractPlugin.loader
+     'css-loader', // 
+     'postcss-loader', //должно добавлять префиксы стилей под разные браузеры
+     'sass-loader' //для обработки препроцессоров scss
+     ]
+   },
+   {
+     test: /\.(img|svg|jpg|jpeg|gif)$/i,
+     type: 'asset/resource',
+   },
+   {
+    test: /\.(woff|woff2|eot|ttf|otf)$/i,
+    type: 'asset/resource',
   },
-  module: {
-    rules: [
-      {
-        test: /\.[tj]s$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-       test: /\.(?:ico|gif|png|jpg|jpeg|svg)$/i,
-       type: 'asset/resource',
-     },
-     {
-       test: /\.(woff(2)?|eot|ttf|otf)$/i,
-       type: 'asset/resource',
-     },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  plugins: [
-   new HtmlWebpackPlugin({
-     title: 'Hello world',
-   }),
-  ]
-};
+ ]
+}
+}
